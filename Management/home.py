cb361b898941a7926e1f36b350458d43c6d3dd72
Martio0910 <mmartinezc0910@hotmail.com>
2020-02-04 21:47:45 -0600

    suma2

--- Management/home.py
+++ Management/home.py
@@ -1,3 +1,4 @@
+# un programa básico que suma 2 enteros
 print('Bienvenido a mi programa de prueba en python')
 print('Vamos a sumar 2 números enteros')
 a = int(input('Introduce el primer número: '))
